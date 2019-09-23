f2fff0757a0e363d202c0a140978f159d7af928e
LAB316 Student02 <sabakevlishvili321@gmail.com>
2019-09-23 18:16:08 +0400

    code

--- src/Gau/Main.java
+++ src/Gau/Main.java
@@ -1,9 +1,27 @@
 package Gau;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello, world!");
+
+        int[] a = new int[4];
+        int[] a1= {1,2,3,4};
+
+        for(int i=0; i < a1.length; i++){
+            System.out.println(a1[i]);
+        }
+
+        Random random = new Random();
+        System.out.println(random.nextInt(100));
+
+        String s;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        System.out.println(s + " nice");
     }
 }
